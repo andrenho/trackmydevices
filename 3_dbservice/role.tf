515366865698d014303886e175f177825c9b33a9
André Wagner <andre.nho@gmail.com>
2019-09-10 08:44:40 -0300

    .

--- 3_dbservice/role.tf
+++ 3_dbservice/role.tf
@@ -1,13 +1,44 @@
-/*
 resource "aws_iam_role_policy" "db_lambda" {
   name = "DatabaseLambdaPolicy"
   role = "${aws_iam_role.db_lambda.id}"
   policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "logs:CreateLogGroup",
+            "Resource": "arn:aws:logs:us-east-1:308301740443:*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "arn:aws:logs:us-east-1:308301740443:log-group:/aws/lambda/database:*"
+        },
+				{
+						"Effect": "Allow",
+						"Action": [
+								"xray:PutTraceSegments",
+								"xray:PutTelemetryRecords"
+						],
+						"Resource": "*"
+				}
+    ]
+}
 EOF
+}
+
+/*
+resource "aws_iam_role_policy_attachment" "execution_role" {
+  role       = "${aws_iam_role.db_lambda.id}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 */
 
-resource "aws_iam_rols" "db_lambda" {
+resource "aws_iam_role" "db_lambda" {
   name = "DatabaseLambdaRole"
   assume_role_policy = <<EOF
 {

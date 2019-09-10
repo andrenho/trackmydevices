c695f1010c004063affb2b95491497b65a3beeb6
André Wagner <andre.nho@gmail.com>
2019-09-10 11:15:19 -0300

    .

--- 3_dbservice/src/lambda_function.py
+++ 3_dbservice/src/lambda_function.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 import psycopg2
 
 db_name = 'tmd'
@@ -6,8 +7,9 @@ db_user = 'tmd'
 db_host = 'db.tmd.gamesmith.co.uk'
 
 def lambda_handler(event, lambda_context):
-    print(event)
-    print(lambda_context)
     password = boto3.client('ssm').get_parameter(Name='db_password', WithDecryption=True)['Parameter']['Value']
     conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (db_name, db_user, db_host, password))
-    return password
+    return {
+        'statusCode': 200,
+        'body': event['pathParameters']['username']
+    }

9d535cc5f4a8faf22a68dc7d629769329127e37d
André Wagner <andre.nho@gmail.com>
2019-09-13 14:02:45 -0300

    .

--- 9_api/api.tf
+++ 9_api/api.tf
@@ -8,59 +8,13 @@ resource "aws_api_gateway_rest_api" "api" {
     types = ["REGIONAL"]
   }
 
-  body =<<EOF
-{
-  "openapi": "3.0.1",
-  "info": {
-    "title": "trackmd",
-    "version": "2019-09-10T14:24:50Z"
-  },
-  "paths": {
-    "/track/{device_id}": {
-      "get": {
-        "consumes": [ "application/json" ],
-        "produces": [ "application/json" ],
-        "parameters": [
-          { "name": "device_id", "in": "path", "required": true, "type": "string" }
-        ],
-        "responses": { "200": { "description": "200 response" } },
-        "x-amazon-apigateway-integration": {
-          "credentials": "arn:aws:iam::308301740443:role/DynamoTrackQueryTemp",
-          "uri": "arn:aws:apigateway:us-east-1:dynamodb:action/Query",
-          "responses": {
-            "2\\d{2}": {
-              "statusCode": "200",
-              "responseTemplates": {
-                "application/json": "#set($root = $input.path('$'))\n[\n#foreach($item in $root.Items)\n    $item\n    #if($foreach.hasNext),#end\n#end\n]\n\n"
-              }
-            }
-          },
-          "requestTemplates": {
-            "application/json": "{\n    \"TableName\": \"track\",\n    \"KeyConditionExpression\": \"device_id = :v1\",\n    \"ExpressionAttributeValues\": {\n        \":v1\": { \"S\": \"$input.params('device_id')\" }\n    }\n}"
-          },
-          "passthroughBehavior": "when_no_templates",
-          "httpMethod": "POST",
-          "type": "aws"
-        }
-      }
-    },
-    "/user/{username}": {
-      "get": {
-        "parameters": [
-          { "name": "username", "in": "path", "required": true, "schema": { "type": "string" } }
-        ],
-        "x-amazon-apigateway-integration": {
-          "uri": "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${data.aws_lambda_function.database.function_name}:prod/invocations",
-          "passthroughBehavior": "when_no_match",
-          "httpMethod": "POST",
-          "contentHandling": "CONVERT_TO_TEXT",
-          "type": "aws_proxy"
-        }
-      }
-    }
-  }
-} 
-EOF
+  body = templatefile("api.json", {
+    region                  = "${data.aws_region.current.name}"
+    account                 = "${data.aws_caller_identity.current.account_id}"
+    user_function_name      = "${data.aws_lambda_function.database.function_name}"
+    track_request_template  = jsonencode(templatefile("track.request.template", {}))
+    track_response_template = jsonencode(templatefile("track.response.template", {}))
+  })
 }
 
 resource "aws_api_gateway_deployment" "prod" {
--- 9_api/data.tf
+++ 9_api/data.tf
@@ -1,3 +1,7 @@
 data "aws_lambda_function" "database" {
   function_name = "database_api"
 }
+
+data "aws_dynamodb_table" "track" {
+  name = "track"
+}
